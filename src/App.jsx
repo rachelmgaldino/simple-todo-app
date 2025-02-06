6f019342290e60bbb7b0f46fce06438bd3427687
Rachel G. <rachel.galdino@outlook.com>
2025-02-05 21:19:20 -0300

    feat: add header and logo, add global styles and font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import styles from "./App.module.css";
+
+import "./global.css";
+
+import logo from "./assets/rocket-logo.svg";
 function App() {
   return (
-    <>
-      <p className="read-the-docs">
-        Cleared app
-      </p>
-    </>
+    <div>
+      <header className={styles.header}>
+        <img src={logo} />
+        <h1>
+          to<span>do</span>
+        </h1>
+      </header>
+    </div>
   );
 }
 
